5326fd77d6b6d5ea0b1e73028e3c201a3bb5fd24
Ramesh <ramesh.senthurpandi@mollie.com>
2024-01-07 19:48:54 +0100

    Modified the slave setup and addition of testing script

--- setup-master.sh
+++ setup-master.sh
@@ -22,6 +22,9 @@ CURRENT_LOG_POS=$(echo "$MASTER_STATUS" | awk '{print $2}')
 echo "Current Binary Log File: $CURRENT_LOG_FILE"
 echo "Current Binary Log Position: $CURRENT_LOG_POS"
 # Save master status to a file for later use in slave configuration
-echo "mysql -uroot -p$MYSQL_ROOT_PASSWORD -e \"CHANGE MASTER TO MASTER_HOST='master', MASTER_PORT=3306, MASTER_USER='repl_user', MASTER_PASSWORD='repl_pass', MASTER_LOG_FILE='mysql-bin.000002', MASTER_LOG_POS=3041454;\"" | tee /shared/master_status.sql
+echo "mysql -uroot -p$MYSQL_ROOT_PASSWORD -e \"CHANGE MASTER TO MASTER_HOST='master', MASTER_PORT=3306, MASTER_USER='repl_user', MASTER_PASSWORD='repl_pass', MASTER_LOG_FILE='$CURRENT_LOG_FILE', MASTER_LOG_POS=$CURRENT_LOG_POS;\"" | tee /shared/master_status.sql
 echo "permission changed to chmod +r"
 chmod +r /shared/master_status.sql
+
+#Test data
+mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "USE $MYSQL_DATABASE; CREATE TABLE t1(c1 TEXT); INSERT INTO t1(c1) VALUES ('Test data');"
--- setup-slave.sh
+++ setup-slave.sh
@@ -12,24 +12,24 @@ mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED
 mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' WITH GRANT OPTION;"
 mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
 
-# Configure replication
-# mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "CHANGE MASTER TO MASTER_HOST='$MYSQL_MASTER_HOST', MASTER_PORT=$MYSQL_MASTER_PORT, MASTER_USER='$MYSQL_REPL_USER', MASTER_PASSWORD='$MYSQL_REPL_PASSWORD', MASTER_LOG_FILE='$MYSQL_MASTER_LOG_FILE', MASTER_LOG_POS=$MYSQL_MASTER_LOG_POS;"
-
 # Print the content of the replication shared script
 echo "printing the output of the replication script"
 cat /shared/master_status.sql
 
+# Wait for 5 seconds
+sleep 5
 
 # Print a message to indicate the start of the shared script execution
 echo "Running shared script from setup-slave.sh"
 
 
-# Invoke the configured replication shared script
-# /shared/shared_script.sh
-/shared/master_status.sql
+# Connect to MySQL and execute SQL and Invoke the configured replication shared script
+source /shared/master_status.sql
 
 # Start the slave
+echo "Starting the slave using: start slave"
 mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "START SLAVE;"
 
 # Display slave status
+echo "Display slave status:show slave status"
 mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "SHOW SLAVE STATUS\G;"
